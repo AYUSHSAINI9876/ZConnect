b4a8a29a4752e844b331eeca206e23c150f82d96
AYUSHSAINI9876 <ayushkumarsiani@gmail.com>
2025-06-19 12:24:50 +0530

    Update environment.js

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-  ? "https://ayushsaini9876backend.onrender.com"
+  ? "https://zmeetup.onrender.com"
   : "http://localhost:8000";
 
 export default server;
