b7f5246bf5c2cf1a84c9ce827e3089a1b72b2c03
AYUSHSAINI9876 <ayushkumarsiani@gmail.com>
2025-06-23 12:53:13 +0530

    Update App.js

--- backend/src/App.js
+++ backend/src/App.js
@@ -6,7 +6,7 @@ import { connectToSocket } from "./controllers/socketManager.js";
 import cors from "cors";
 // ✅ Corrected import path (lowercase 'routes')
 import userRoutes from "./routes/users.routes.js"; // ✅ lowercase
-
+import bcrypt from "bcryptjs";
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
