9b6e35e77c22e6d171bf4a8b7434013b6a982ad4
AYUSHSAINI9876 <ayushkumarsiani@gmail.com>
2025-06-23 12:43:11 +0530

    Update App.js

--- backend/src/App.js
+++ backend/src/App.js
@@ -4,7 +4,6 @@ import { Server } from "socket.io";
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
 import cors from "cors";
-import bcrypt from "bcryptjs";
 // ✅ Corrected import path (lowercase 'routes')
 import userRoutes from "./routes/users.routes.js"; // ✅ lowercase
 
