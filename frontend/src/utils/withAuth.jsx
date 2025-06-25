028ab6a88e897227bffe84bfa97f428453693e6e
AYUSHSAINI9876 <ayushkumarsiani@gmail.com>
2025-06-25 19:52:05 +0530

    Update withAuth.jsx

--- frontend/src/utils/withAuth.jsx
+++ frontend/src/utils/withAuth.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom"
-
 const withAuth = (WrappedComponent ) => {
     const AuthComponent = (props) => {
         const router = useNavigate();
